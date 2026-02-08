4bb66a8d6866654fd001075dfd8e5bcddd11c4bc
C.O Sistemas <caiosan321@icloud.com>
2026-02-08 17:05:09 -0300

    Update betController.js

--- controllers/betController.js
+++ controllers/betController.js
@@ -1,32 +1,24 @@
 const pool = require('../db');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
-exports.register = async (req, res) => {
-  const { name, email, password } = req.body;
-  const hash = await bcrypt.hash(password, 10);
+exports.placeBet = async (req, res) => {
+  const { odd_id, amount } = req.body;
+
+  const odd = await pool.query(
+    'SELECT odd FROM odds WHERE id=$1',
+    [odd_id]
+  );
+
+  const possible = amount * odd.rows[0].odd;
 
   await pool.query(
-    'INSERT INTO users (name,email,password) VALUES ($1,$2,$3)',
-    [name, email, hash]
+    'INSERT INTO bets (user_id, odd_id, amount, possible_return) VALUES ($1,$2,$3,$4)',
+    [req.user.id, odd_id, amount, possible]
   );
 
-  res.json({ message: 'Usuário criado' });
-};
-
-exports.login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await pool.query('SELECT * FROM users WHERE email=$1', [email]);
-
-  if (!user.rows[0]) return res.sendStatus(401);
-
-  const valid = await bcrypt.compare(password, user.rows[0].password);
-  if (!valid) return res.sendStatus(401);
-
-  const token = jwt.sign(
-    { id: user.rows[0].id, role: user.rows[0].role },
-    process.env.JWT_SECRET
+  await pool.query(
+    'UPDATE users SET balance = balance - $1 WHERE id=$2',
+    [amount, req.user.id]
   );
 
-  res.json({ token });
+  res.json({ message: 'Aposta realizada', possible });
 };
